b28cfa886985e6308fc97e0d3a24259865150887
yk_marble <milkcocoapple@gmail.com>
2016-02-27 01:31:12 +0900

    add周りのUXを向上させた.

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,42 @@ function getfakelocation() {
 }
 
 $(".itementry").click(function () {
-    $.post("/queue/add", {path: getfakelocation() + $(this).find(".button-text").text()}, function (data) {
-        alert(data)
-    })
+    var clicked_dom = this;
+    $.post("/queue/add",
+           {path: getfakelocation() + $(this).find(".button-text").text()},
+           function (data) {
+               $(clicked_dom).find(".regular-icon").css("display", "inline-block");
+               $(clicked_dom).find(".loading-icon").css("display", "none");
+               var toast = new Toast();
+               toast.show(data)
+           })
 })
+
+$(".list-button").click(function () {
+    $(this).find(".regular-icon").css("display", "none");
+    $(this).find(".loading-icon").css("display", "inline-block");
+})
+
+var Toast = (function(){
+    var timer;
+    var speed;
+    function Toast() {
+        this.speed = 3000;
+    }
+    // メッセージを表示。表示時間(speed)はデフォルトで3秒
+    Toast.prototype.show = function(message, speed) {
+        if (speed === undefined) speed = this.speed;
+        $('.toast').remove();
+        clearTimeout(this.timer);
+        $('body').append('<div class="toast">' + message + '</div>');
+        var leftpos = $('body').width()/2 - $('.toast').outerWidth()/2;
+        $('.toast').css('left', leftpos).hide().fadeIn('fast');
+
+        this.timer = setTimeout(function(){
+            $('.toast').fadeOut('slow',function(){
+                $(this).remove();
+            });
+        }, speed);
+    };
+    return Toast;
+})();
